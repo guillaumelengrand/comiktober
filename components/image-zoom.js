03b2ffd6f143b72625403c9c2b144b3a87e97048
Guillaume Lengrand <lengra_g@epita.fr>
2021-10-08 13:45:58 +0200

    Tailwind: fix bug that remove tailwind class

--- components/image-zoom.js
+++ components/image-zoom.js
@@ -7,11 +7,11 @@ export default function ImageZoom({src, alt, isWinner}) {
     };
     var winnerCss = '';
     if (isWinner) {
-        winnerCss = ' border-4 border-green-600';
+        winnerCss = 'border-4 border-green-600';
     }
     return (
         <div>
-            <img className={`max-h-96${winnerCss}`} src={src} alt={alt} onClick={onClick} />
+            <img className={`max-h-96 ${winnerCss}`} src={src} alt={alt} onClick={onClick} />
 
             {isZoom && (
                 <div className="fixed top-0 left-0 w-screen max-h-screen bg-black bg-opacity-50" onClick={onClick}>
